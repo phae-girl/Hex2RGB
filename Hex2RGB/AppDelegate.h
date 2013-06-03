a3948534bfd6b449a4551980655c8af71de02316
Phaedra Deepsky <phae.deepsky@gmail.com>
2013-06-03 05:11:51 -0400

    First Commit - Basic Working App

--- Hex2RGB/AppDelegate.h
+++ Hex2RGB/AppDelegate.h
@@ -11,5 +11,19 @@
 @interface AppDelegate : NSObject <NSApplicationDelegate>
 
 @property (assign) IBOutlet NSWindow *window;
+@property (weak) IBOutlet NSTextField *hexValue;
+
+@property (weak) IBOutlet NSTextField *floatRed;
+@property (weak) IBOutlet NSTextField *floatGreen;
+@property (weak) IBOutlet NSTextField *floatBlue;
+
+@property (weak) IBOutlet NSTextField *integerRed;
+@property (weak) IBOutlet NSTextField *integerGreen;
+@property (weak) IBOutlet NSTextField *integerBlue;
+
+
+- (IBAction)convert:(id)sender;
+- (IBAction)copyFloats:(id)sender;
+- (IBAction)copyIntegers:(id)sender;
 
 @end
